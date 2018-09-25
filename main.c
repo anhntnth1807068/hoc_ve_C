2ea910df93527e8f98865583f15e235d854dfb1a
Ngo Anh <anhntnth1807068@fpt.edu.vn>
2018-09-25 16:37:17 +0700

    su dung typedef

--- main.c
+++ main.c
@@ -1,23 +1,30 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-struct pig{
+typedef struct {
+    char city[50];
+    char district[50];
+} address;
+typedef struct {
     int pig_id;
     char pig_name[50];
     int pig_age;
     int pig_weight;
-};
-
-int main() {
-    struct pig mypig;
-    mypig.pig_id = 1;
-    strcpy(mypig.pig_name, "lon nai");
-    mypig.pig_age = 10;
-    mypig.pig_weight = 30;
-    printf("ma lonla:%d\n", mypig.pig_id);
-    printf("ten lonla:%s\n", mypig.pig_name);
-    printf("tuoi lonla:%d\n", mypig.pig_age);
-    printf("can lonla:%d\n", mypig.pig_weight);
+    address address;
+}piggy;
+int main(){
+    piggy mypig;
+    mypig.pig_id =1;
+    strcpy(mypig.pig_name, "lua t");
+    mypig.pig_age =10;
+    mypig.pig_weight=30;
+    strcpy(mypig.address.city,"thai boinh");
+    strcpy(mypig.address.district,"thai xuyen");
+    printf("ma lonla:%d\n",mypig.pig_id);
+    printf("ten lonla:%d\n",mypig.pig_name);
+    printf("tuoi lonla:%d\n",mypig.pig_age);
+    printf("can lonla:%d\n",mypig.pig_weight);
+    printf("thanhpho lonla:%s\n",mypig.address.city);
+    printf("huynlonla:%s\n",mypig.address.district);
     return 0;
 }
